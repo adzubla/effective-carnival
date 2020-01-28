2fd3165192464a361c9805b0d9861d046e0aed25
Eduardo Ito <eduardo.ito@gmail.com>
2020-01-28 17:49:05 -0300

    Inicio

---
+++ test-junit5/src/test/java/com/example/calc/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.example.calc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator();
+        assertEquals(3, calculator.add(1, 2));
+        assertEquals(3, calculator.add(2, 1));
+    }
+
+    @Test
+    void subtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(1, 2));
+        assertEquals(1, calculator.subtract(2, 1));
+    }
+
+}
