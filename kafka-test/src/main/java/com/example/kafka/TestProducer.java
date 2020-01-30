96757cc7d45822d42a37d787dc7544375b31653e
Eduardo Ito <eduardo.ito@gmail.com>
2020-01-30 14:51:18 -0300

    kafka-test

---
+++ kafka-test/src/main/java/com/example/kafka/TestProducer.java
@@ -0,0 +1,25 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class TestProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<>("my-topic", "k-" + i, "v-" + i));
+        }
+        producer.close();
+    }
+
+}
