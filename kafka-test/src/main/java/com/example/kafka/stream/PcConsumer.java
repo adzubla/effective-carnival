4299548dc2cd7f40dee280db3845ff43af3aa9aa
Eduardo Ito <eduardo.ito@gmail.com>
2020-02-05 14:09:32 -0300

    Rename

--- kafka-test/src/main/java/com/example/kafka/stream/PcConsumer.java
+++ kafka-test/src/main/java/com/example/kafka/stream/PcConsumer.java
@@ -1,4 +1,4 @@
-package com.example.kafka.pc;
+package com.example.kafka.stream;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
--- kafka-test/src/main/java/com/example/kafka/stream/PcStream.java
+++ kafka-test/src/main/java/com/example/kafka/stream/PcStream.java
@@ -1,4 +1,4 @@
-package com.example.kafka.pc;
+package com.example.kafka.stream;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
